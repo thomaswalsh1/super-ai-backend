86f658988be4dfd6c234facd0bdd686b50e37f0a
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2024-11-09 00:02:48 -0500

    Add compare route

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/compare")
+async def compare_responses():
+    return {"message": "Comparing..."}
